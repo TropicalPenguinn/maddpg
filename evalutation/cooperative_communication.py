706f43db677cc40546ec9c50af0bb37fbac267bf
Tropical Penguin <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2022-07-15 10:02:43 +0900

    Update cooperative_communication.py

--- evalutation/cooperative_communication.py
+++ evalutation/cooperative_communication.py
@@ -3,18 +3,33 @@ import numpy as np
 import pandas as pd
 import pickle
 import scipy.stats as stats
+import seaborn as sns
 
 
+def reward_plot(experiments):
+    reward_lst=[]
+    for experiment in experiments:
+        file = open('/home/airlab/PycharmProjects/maddpg2/experiments/simple_speaker_listener_' +experiment+ '/agrewards.pkl', 'rb')
+        data = pickle.load(file)
+        file.close()
+        ag = [data[2*n] for n in range(int(len(data)/2))]
+        reward_lst.append(ag)
 
-def reward_plot(experiment):
-    file = open('/home/airlab/PycharmProjects/maddpg2/experiments/simple_speaker_listener_' +experiment+ '/rewards.pkl', 'rb')
-    data = pickle.load(file)
-    file.close()
-    ag = [data[n] for n in range(int(len(data)))]
-    x = np.arange(len(ag))
-    plt.plot(x, ag, 'b')
+    x = np.arange(len(reward_lst[0]))
+
+    csv_algo_list = list()
+    csv_inter_list = list()
+    csv_reward_list = list()
+
+    for i in range(3):
+        name=["maddpg" for _ in range(len(reward_lst[0]))]
+        csv_algo_list.extend(name)
+        csv_inter_list.extend(x)
+        csv_reward_list.extend(reward_lst[i])
+
+    my_df = pd.DataFrame({"Model": csv_algo_list, "episodes": csv_inter_list, "mean reward": csv_reward_list})
+    sns.lineplot(x="episodes", y="mean reward", hue="Model", data=my_df)
     plt.show()
-
 
 def benchmark_data(benchmark):
     file = open('/home/airlab/PycharmProjects/maddpg2/experiments/simple_speaker_listener_' + benchmark + '/benchmark.pkl',
@@ -38,6 +53,9 @@ def combine_benchmarks(benchmarks):
 
 
 def main():
+    plt.title("maddpg vs. maddpg")
+    reward_plot(['mm','mm1','mm2'])
+
     table2 = pd.DataFrame([['maddpg'] + combine_benchmarks(['mm',
                                                             'mm1',
                                                             'mm2'
@@ -48,10 +66,11 @@ def main():
                                    'Average distance'
                                    ])
     print(table2)
-
+    table2.to_csv('cooperative_communication.csv',index=False)
 
 if __name__=="__main__":
     main()
+
 
 
 
